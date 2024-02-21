eb27cdf8f6227b1b6ec21ce4d3d575f4ba3ac511
colton <cmpadden@users.noreply.github.com>
2024-02-21 17:10:20 +0000

    Add Example Assets (#1)
    * add hacker news example; add gitignore
    
    * remove dependency on venv for uv, as we're dockerized
    
    * include instructions on opening webpage in codespaces
    
    * rephrase tagline

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     packages=find_packages(exclude=["dagster_quickstart_tests"]),
     install_requires=[
         "dagster",
-        "dagster-cloud"
+        "dagster-cloud",
+        "pandas",
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
