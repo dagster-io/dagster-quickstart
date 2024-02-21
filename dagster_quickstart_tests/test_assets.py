20ee1b73deefd393b61bc867cbca4d3c8ab2f6a7
colton <cmpadden@users.noreply.github.com>
2024-02-21 21:28:23 +0000

    [feat] add unit test example; refactor to use hn config; add ruff config (#2)
    * add hn story config; add ruff config
    
    * add unit test for hackernews_top_stories

--- dagster_quickstart_tests/test_assets.py
+++ dagster_quickstart_tests/test_assets.py
@@ -1,1 +1,32 @@
+import os
+import unittest
 
+from dagster_quickstart.assets import hackernews_top_stories
+from dagster_quickstart.configurations import HNStoriesConfig
+from unittest import mock
+
+
+@mock.patch("requests.get")
+def test_hackernews_top_stories(mock_get):
+    mock_response = mock.Mock()
+    mock_response.json.return_value = {
+        "title": "Mock Title",
+        "by": "John Smith",
+        "url": "www.example.com",
+    }
+    mock_get.return_value = mock_response
+
+    config = HNStoriesConfig(
+        hn_top_story_ids_path="dagster_quickstart_tests/hackernews_top_story_ids.json",
+        hn_top_stories_path="dagster_quickstart_tests/hackernews_top_stories.csv",
+    )
+
+    materialized_results = hackernews_top_stories(config)
+
+    assert materialized_results.metadata.get("num_records") == 10
+
+    os.remove("dagster_quickstart_tests/hackernews_top_stories.csv")
+
+
+if __name__ == "__main__":
+    unittest.main()
