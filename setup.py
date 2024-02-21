9af93568e3af8cf3f7432aff3c9b2e55e15046d0
Colton Padden <colton@dagsterlabs.com>
2024-02-21 09:24:32 -0500

    dagster project scaffold --name dagster-quickstart

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dagster_quickstart",
+    packages=find_packages(exclude=["dagster_quickstart_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
